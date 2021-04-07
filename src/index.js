2f58e84fc62dc4e922a910da4f2998b7bb0d72ea
Juan Gomez <jf.gomezramiez@gmail.com>
2021-04-06 22:09:26 -0500

    Finalizado el primer taller

--- src/index.js
+++ src/index.js
@@ -6,6 +6,19 @@
 const baseUrl = "https://platzi-avo.vercel.app"
 
 const appNode = document.querySelector('#app');
+
+const formatPrice = (price) =>{
+
+    const newPrice = window.Intl.NumberFormat('en-EN', {
+        style: 'currency',
+        currency: 'USD',
+    }).format(price)
+
+    return newPrice;
+};
+
+//API INTL
+
 /*
 WEB API
 
@@ -28,24 +41,34 @@ window.
             const imagen = document.createElement('img');
             //url de la imagen
             imagen.src = `${baseUrl}${item.image}`;
+            imagen.className = "h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6";
+
             
             //crear titulo
             const title = document.createElement('h2');
             title.textContent = item.name;
+            title.className = 'text-lg';
             
             //crear precio
             const price = document.createElement('div');
-            price.textContent = item.price;
+            price.textContent = formatPrice(item.price);
+            price.className = 'text-gray-600';
             
-            //creo un contenedor para que cada información de cada item se vea más organizada
-            const container = document.createElement('div');
-            container.append(imagen, title, price);
+            //Wrap price & title
+            const priceAndTitle = document.createElement('div')
+            priceAndTitle.className = 'text-center md:text-left'
+            priceAndTitle.append(title, price)
 
-            todosLosItems.push(container);
-        });
+            //Wrap Img and priceAndTitle
+            const card = document.createElement('div')
+            card.className = "md:flex bg-white rounded-lg p-6 hover:bg-gray-300"
+            card.append(imagen, priceAndTitle)
+            console.log(card)
 
-        appNode.append(...todosLosItems);
-    });
+            todosLosItems.push(card)
+        })
+        appNode.append(...todosLosItems)
+    })
 
 //También se puede utilizar async/await pero ya es como aprendizaje personal
 
